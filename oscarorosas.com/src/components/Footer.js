710e8ae115cc9a90e8264802dac59ad737ec47c6
Oscar <rosasoscaro@gmail.com>
2025-02-19 14:45:26 -0500

    Add footer component with styling and update navbar logo; include new dependencies.
    Note: Fix dead space on right side of screen (scroll).

--- oscarorosas.com/src/components/Footer.js
+++ oscarorosas.com/src/components/Footer.js
@@ -1,37 +1,40 @@
-import "./Footer.css"
+import "./Footer.css";
 
 const Footer = () => {
     return (
-      <footer>
-    <div class="footer-container">
-        <div class="footer-left">
-            <h2>Wordcraft Webworks</h2>
-            <p>Building stunning websites that convert.</p>
-        </div>
-        <div class="footer-center">
-            <h3>Quick Links</h3>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-        <div class="footer-right">
-            <h3>Subscribe</h3>
-            <form>
-                <input type="email" placeholder="Your email">
-                <button type="submit">Subscribe</button>
-            </form>
-            <div class="social-icons">
-                <a href="#"><img src="/icons/facebook.svg" alt="Facebook"></a>
-                <a href="#"><img src="/icons/twitter.svg" alt="Twitter"></a>
-                <a href="#"><img src="/icons/instagram.svg" alt="Instagram"></a>
+        <footer>
+            <div className="footer-container">
+                <div className="footer-left">
+                    <h2>Oscar O. Rosas</h2>
+                    <br></br>
+                    <p>"They named things after me for a reason."</p>
+                </div>
+                <div className="footer-center">
+                    <h3>Quick Links</h3>
+                    <br></br>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="footer-right">
+                    <h3>Subscribe</h3>
+                    <br></br>
+                    <form>
+                        <input type="email" placeholder="Your email" />
+                        <button type="submit">Subscribe</button>
+                    </form>
+                    <div className="social-icons">
+                        <a href="https://www.facebook.com/profile.php?id=100019522081506"><img src="/icons/facebook.svg" alt=" Facebook"/></a>
+                        <a href="https://www.instagram.com/theoscar.r/"><img src="/icons/instagram.svg" alt=" Instagram"/></a>
+                        <a href="https://wa.me/8452340502"><img src="/icons/whatsapp.svg" alt=" Whatsapp"/></a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</footer>
-  );
+        </footer>
+    );
 };
 
-export default Footer
+export default Footer;
--- oscarorosas.com/src/components/NavBar.js
+++ oscarorosas.com/src/components/NavBar.js
@@ -5,8 +5,7 @@ import {ShoppingCartOutlined} from '@ant-design/icons'
 const NavBar = () => {
   return (
     <nav className="navbar">
-      {<img src="#" alt="logo-image"></img>}
-     
+      {<img className="logo" src="https://pbs.twimg.com/media/Ggom8rkXcAAW8Xt?format=jpg&name=large" alt="logo-image"></img>}
       <p className="navbar-heading navbar-icons">oscarorosas.com</p>
       <ShoppingCartOutlined className="navbar-icons"/>
     </nav>
