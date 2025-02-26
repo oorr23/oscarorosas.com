8405413b7a41309d9bf9147f1690790b047c5cab
Oscar <rosasoscaro@gmail.com>
2025-02-26 09:55:15 -0500

    Minor retouches (footer, links), made Cart & linked to it.

--- oscarorosas.com/src/App.js
+++ oscarorosas.com/src/App.js
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import Home from "./components/Home";
 import CounterSlice from "./components/counterComponent";
+import Cart from "./components/Cart";
 function App() {
   const hasSeenPopup = localStorage.getItem("hasSeenPopup");
   return (
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home hasSeenPopup = {hasSeenPopup}/>} />
         <Route path="/counter" element={<CounterSlice />} />
+        <Route path="/cart" element = {<Cart/>} ></Route>
       </Routes>
     </BrowserRouter>
   );
--- oscarorosas.com/src/components/NavBar.js
+++ oscarorosas.com/src/components/NavBar.js
@@ -1,13 +1,15 @@
 import "./NavBar.css"
 import {ShoppingCartOutlined} from '@ant-design/icons'
+import { useNavigate } from "react-router-dom";
 
 
 const NavBar = () => {
+  const navigate = useNavigate()
   return (
     <nav className="navbar">
       {<img className="logo" src="https://pbs.twimg.com/media/Ggom8rkXcAAW8Xt?format=jpg&name=large" alt="logo-image"></img>}
       <p className="navbar-heading navbar-icons">oscarorosas.com</p>
-      <ShoppingCartOutlined className="navbar-icons"/>
+      <ShoppingCartOutlined className="navbar-icons" onClick={() => navigate("/Cart")}/>
     </nav>
   );
 };
